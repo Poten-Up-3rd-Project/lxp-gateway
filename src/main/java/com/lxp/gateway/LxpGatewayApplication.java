a99144592595f88a51bce243eed00d65515c0292
dnya0 <annay01794@gmail.com>
2025-12-28 18:04:05 +0900

    chore: initialize project

---
+++ src/main/java/com/lxp/gateway/LxpGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lxp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxpGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxpGatewayApplication.class, args);
+    }
+
+}

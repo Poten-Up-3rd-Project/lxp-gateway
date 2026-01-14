4e93c33cbc3c2e5efb1f9bb7aa0cc173b2b8deb3
dnya0 <annay01794@gmail.com>
2026-01-14 11:55:32 +0900

    feat: add security configuration with permit all access

---
+++ src/main/java/com/lxp/gateway/global/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.lxp.gateway.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+            .authorizeExchange(exchanges -> exchanges
+                .anyExchange().permitAll()
+            )
+            .build();
+    }
+
+}

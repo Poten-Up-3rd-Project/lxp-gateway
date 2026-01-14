544e7a4a0c23f083e90b86a2416e7f99fc7f41ff
dnya0 <annay01794@gmail.com>
2026-01-14 11:57:07 +0900

    feat: add configuration annotation to SecurityConfig class

--- src/main/java/com/lxp/gateway/global/config/SecurityConfig.java
+++ src/main/java/com/lxp/gateway/global/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.lxp.gateway.global.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
+@Configuration
 public class SecurityConfig {
 
     @Bean

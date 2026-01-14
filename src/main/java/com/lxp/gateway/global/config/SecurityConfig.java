66534e0e6fb68b97e839548c810ac867e6900d4e
dnya0 <annay01794@gmail.com>
2026-01-14 11:57:49 +0900

    feat: enable WebFlux security in SecurityConfig class

--- src/main/java/com/lxp/gateway/global/config/SecurityConfig.java
+++ src/main/java/com/lxp/gateway/global/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.lxp.gateway.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
